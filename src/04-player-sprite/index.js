1684818dce9c93d7ad6ddd4cd78b85265a14d6a1
竹内佑介 <kaidouji85@gmail.com>
2018-02-03 14:50:05 +0900

    プレイヤースプライトを追加した

---
+++ src/04-player-sprite/index.js
@@ -0,0 +1,19 @@
+import {Player} from './player';
+
+window.onload = function() {
+  const playerSprite = new Player();
+  playerSprite.getDomList().forEach(v => {
+    document.body.appendChild(v);
+  });
+  playerSprite.move().start();
+
+  function gameLoop(time) {
+    requestAnimationFrame(gameLoop);
+    playerSprite.gameLoop(time);
+  }
+  requestAnimationFrame(gameLoop);
+
+  document.onclick = () => {
+    playerSprite.stop();
+  }
+};
