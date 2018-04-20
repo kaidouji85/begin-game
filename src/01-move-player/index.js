7d431876d7ce44099b85aee48c67c38eec336651
竹内佑介 <kaidouji85@gmail.com>
2018-04-20 12:48:27 +0900

    サンプル1をアップデートした

--- src/01-move-player/index.js
+++ src/01-move-player/index.js
@@ -1,14 +1,16 @@
+// プレイヤー関連
 let playerPos = {x: 0, y: 128};
-
-function gameLoop(time) {
-  requestAnimationFrame(gameLoop);
-  playerPos.x += 4;
-  setPlayerPosition(playerPos);
-}
-requestAnimationFrame(gameLoop);
 
 function setPlayerPosition({x, y}) {
   const playerImg = document.querySelector('.player');
   const transform = `translate(${x}px, ${y}px)`;
   playerImg.style.setProperty('transform', transform);
 }
+
+// ゲームループ関連
+function gameLoop(time) {
+  requestAnimationFrame(gameLoop);
+  playerPos.x += 4;
+  setPlayerPosition(playerPos);
+}
+requestAnimationFrame(gameLoop);
