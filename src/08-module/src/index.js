4dbb890385724637c91871e9f6e1b45f022567ab
竹内佑介 <kaidouji85@gmail.com>
2018-06-02 14:06:28 +0900

    当たり判定関数を別ファイルに切り出した

--- src/08-module/src/index.js
+++ src/08-module/src/index.js
@@ -1,23 +1,14 @@
 // ベクトル関係
-import {getScala} from "./vector/scala";
 import {Player} from "./sprite/player";
 import {Enemy} from './sprite/enemy';
 import {TouchInfo} from "./touch/touch-info";
+import {isOverlap} from "./overlap/is-overlap";
 
 const player = new Player();
 const enemy = new Enemy(window.innerWidth - 256, 128);
 const touchInfo = new TouchInfo();
 
-// 当たり判定関連
-function isOverlap(player, enemy) {
-  const vx = player.x - enemy.x;
-  const vy = player.y - enemy.y;
-  const distance = getScala(vx, vy);
-
-  return distance <= 126;
-}
-
-// ゲームループ関連
+// ゲームループ
 function gameLoop(time) {
   requestAnimationFrame(gameLoop);
 
