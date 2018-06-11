9aa1b7f864446bef20ee4f3a7d330bc54d2428b3
竹内佑介 <kaidouji85@gmail.com>
2018-06-11 12:49:56 +0900

    敵の動きを複雑にした

--- src/11-tween/src/sprite/enemy/move.js
+++ src/11-tween/src/sprite/enemy/move.js
@@ -1,10 +1,31 @@
-import {Tween} from "@tweenjs/tween.js/src/Tween";
-
-export const ENEMY_SPEED = 4;
+import {Tween, Easing} from "@tweenjs/tween.js/src/Tween";
 
 export function move(enemy, group) {
-  const tween = new Tween(enemy, group);
-  tween.to({x: '-300'}, 1500)
+  const start = new Tween({}, group);
+  start
+    .to({}, 0)
+    .chain(xAxis(enemy, group), yAxis(enemy, group));
+  return start;
+}
+
+function xAxis(enemy, group) {
+  const first = new Tween(enemy, group);
+  first.to({x: '-300'}, 1500)
     .repeat(Infinity);
-  return tween;
+
+  return first;
+}
+
+function yAxis(enemy, group) {
+  const first = new Tween(enemy, group);
+  const second = new Tween(enemy, group);
+
+  first.to({y: '+400'}, 1500)
+    .chain(second)
+    .easing(Easing.Quintic.InOut);
+  second.to({y: '-400'}, 1500)
+    .chain(first)
+    .easing(Easing.Quadratic.InOut);
+
+  return first;
 }
